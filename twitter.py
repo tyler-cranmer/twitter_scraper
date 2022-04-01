12b841589f2e1180f6b9704fc0f3b0d0201e8dce
tyler-cranmer <tyler.cranmer@outlook.com>
2022-04-01 10:57:23 -0700

    get user info

--- twitter.py
+++ twitter.py
@@ -1,28 +1,21 @@
 import os
 import tweepy
-from dotenv import load_dotenv
+import config
 
-load_dotenv()
 
-API_KEY = os.getenv('TWITTER_API_KEY')
-API_SECRET_KEY = os.getenv('API_SECRET_KEY')
-BEARER_TOKEN = os.getenv('BEARER_TOKEN')
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+def getClient():
+    client = tweepy.Client(bearer_token=config.BEARER_TOKEN,
+                        consumer_key=config.TWITTER_API_KEY, 
+                        consumer_secret=config.TWITTER_API_SECRET_KEY, 
+                        access_token=config.ACCESS_TOKEN, 
+                        access_token_secret=config.ACCESS_TOKEN_SECRET,)
+    return client
 
-def scrape_user_followers(username):
-    followers_scraped = []
-    user = api.get_user(username)
-    for i, _id in enumerate(tweepy.Cursor(api.followers_id, screen_name = username).items()):
-        print(i, _id)
-        followers_scraped.append(_id)
-    return followers_scraped
+def getUserInfo(username: str):
+    client = getClient()
+    user = client.get_user(username=username)
+    return user.data
 
 if __name__ == '__main__':
 
-    client = tweepy.Client(bearer_token=BEARER_TOKEN)
-    screenName = 52064008
-    # response = client.get_users(usernames=['Iamamystreet'])
-    # followers = client.get_users_followers(id=screenName, max_results=1000, pagination_token=next_token)   
-    # l = []    
-    scrape_user_followers(screenName) 
+   print(getUserInfo('tyler_cranmer'))
