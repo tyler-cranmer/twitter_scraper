dc8e6ef99bab0c53a44a852fdea8a4cc10b65c21
tyler-cranmer <tyler.cranmer@outlook.com>
2022-04-02 18:37:37 -0700

    getAllFollowers

--- twitter.py
+++ twitter.py
@@ -30,26 +30,6 @@ def getFollowers(_id: int, _max_results: int) -> list:
 
 tyler_id = 4286418493
 
-def pagFollowers(_id: int, _max_results: int, _pagination:str):
-    client: object = getClient()
-    users: object = client.get_users_followers(id=_id, max_results=_max_results, pagination_token= _pagination)
-    followers: list = []
-    dic: dict = {}
-    if 'next_token' not in users.meta:
-        next_token: str = ''
-    else: 
-        next_token = users.meta['next_token']
-    for user in users.data:
-        dic = {}
-        dic.update({"userName": user.username})
-        dic.update({"userID": str(user.id)})
-        followers.append(dic)
-
-    if len(next_token) != 0:
-        return pagFollowers(_id, _max_results, next_token)
-    else:
-        return followers
-
 def writeJson(filepath: str, data: list) -> json:
     with open(filepath, 'w') as fp:
         json.dump(data,fp)
@@ -58,7 +38,7 @@ def readJson(filename: str) -> json:
     with open(filename, 'r', encoding='utf-8') as fp:
         return json.load(fp)
 
-def getAllFollowers(_id: int, _max_results: int):
+def getAllFollowers(_id: int, _max_results: int) -> list:
     client: object = getClient()
     users: object = client.get_users_followers(id=_id, max_results=_max_results)
     followers: list = []
@@ -68,19 +48,14 @@ def getAllFollowers(_id: int, _max_results: int):
             dic.update({"userName": user.username})
             dic.update({"userID": str(user.id)})
             followers.append(dic)
-        users = client.get_users_followers(id=_id, max_results=_max_results, pagination_token= users.meta['next_token'])
         if('next_token' not in users.meta):
-            for user in users.data:
-                dic = {}
-                dic.update({"userName": user.username})
-                dic.update({"userID": str(user.id)})
-                followers.append(dic)
             break
+        users = client.get_users_followers(id=_id, max_results=_max_results, pagination_token= users.meta['next_token'])
     return followers
         
 
 if __name__ == '__main__':
 
-    pag = getAllFollowers(tyler_id, 25)
+    pag = getAllFollowers(tyler_id, 100)
     print(pag)
     print(len(pag))
